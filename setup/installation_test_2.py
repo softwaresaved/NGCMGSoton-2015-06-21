ac22d8690c722955aa1934e98606f843febd36ce
Steve Crouch <s.crouch@software.ac.uk>
2015-06-09 13:26:17 +0100

    Comment out checks for those tools we don't need

--- setup/installation_test_2.py
+++ setup/installation_test_2.py
@@ -70,35 +70,35 @@ CHECKS = [
     'virtual-browser',
 # Version control
     'git',
-    'hg',              # Command line tool
+    #'hg',              # Command line tool
     #'mercurial',       # Python package
-    'EasyMercurial',
+    #'EasyMercurial',
 # Build tools and packaging
-    'make',
+    #'make',
     'virtual-pypi-installer',
     'setuptools',
     #'xcode',
 # Testing
     'nosetests',       # Command line tool
     'nose',            # Python package
-    'py.test',         # Command line tool
-    'pytest',          # Python package
+    #'py.test',         # Command line tool
+    #'pytest',          # Python package
 # SQL
-    'sqlite3',         # Command line tool
-    'sqlite3-python',  # Python package
+    #'sqlite3',         # Command line tool
+    #'sqlite3-python',  # Python package
 # Python
     'python',
-    'ipython',         # Command line tool
-    'IPython',         # Python package
+    #'ipython',         # Command line tool
+    #'IPython',         # Python package
     'argparse',        # Useful for utility scripts
     'numpy',
-    'scipy',
+    #'scipy',
     'matplotlib',
-    'pandas',
-    'sympy',
-    'Cython',
-    'networkx',
-    'mayavi.mlab',
+    #'pandas',
+    #'sympy',
+    #'Cython',
+    #'networkx',
+    #'mayavi.mlab',
     ]
 
 CHECKER = {}
