80499fdd86006dcf31036234dcded908e709669a
Steve Crouch <s.crouch@software.ac.uk>
2015-06-19 13:08:05 +0100

    Remove unnecessary files

--- novice/python-unit-testing/code/something.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-def something(items):
-    result = []
-    for i in range(len(items)):
-       for j in range(i+1, len(items)):
-           result.append((items[i], items[j]))
-    return result
